66858ea513e83b966025ebe43cd280cfe4bbd25b
luise1030 <chien-hao.chen@amd.com>
2023-12-14 04:34:34 +0000

    Rename scripts

---
+++ perf_collection/run_and_collect_flops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+input=$1
+outdir_pref=${2:-"./tmp"}
+CURRENTDATE=`date +"%Y-%m-%d-%T"`
+fSummary=${outdir_pref}/summary_${outdir_pref}_${CURRENTDATE}.csv
+
+mkdir ${outdir_pref}
+
+cmds=(`cat $input`)
+exec < $input
+array=()
+id=0
+while read line
+do
+    outdir=${outdir_pref}/test_${id}
+    mkdir ${outdir}
+    fProf=${outdir}/rocprof_test_${id}.csv
+    fStats=${outdir}/rocprof_test_${id}.stats.csv
+    fPerf=${outdir}/perf_test_${id}.csv
+    fCmd=${outdir}/cmd_test_${id}.sh
+    rocprof --stats -o ${fProf} $line
+    $line | grep "Gflops\|GFLOPs" -A 1 | tee ${fPerf}
+    echo $line > ${fCmd}
+    if [[ $id == "0" ]]; then
+        cmd_metric="TestCommand"
+        perf_metric=`head -n 1 ${fPerf} | sed -e 's/\r//g'`
+        kernel_metric=`head -n 1 ${fStats} | sed -e 's/\r//g'`
+        echo "${cmd_metric},${perf_metric},${kernel_metric}" > ${fSummary}
+    fi
+    cmd_data=`tail -n 1 ${fCmd} | sed -e 's/\r//g'`
+    perf_data=`tail -n 1 ${fPerf} | sed -e 's/\r//g'`
+    kernel_data=`head -n 2 ${fStats} | tail -n 1 | sed -e 's/\r//g'`
+    echo "\"$cmd_data\",${perf_data},${kernel_data}" | tee -a ${fSummary}
+    id=$((id+1))
+done
---
+++ perf_collection/run_and_collect_lstm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+input=$1
+outdir_pref=${2:-"./tmp"}
+CURRENTDATE=`date +"%Y-%m-%d-%T"`
+fSummary=${outdir_pref}/summary_${outdir_pref}_${CURRENTDATE}.csv
+
+mkdir ${outdir_pref}
+
+cmds=(`cat $input`)
+exec < $input
+array=()
+id=0
+while read line
+do
+    outdir=${outdir_pref}/test_${id}
+    mkdir ${outdir}
+    fPerf=${outdir}/perf_test_${id}.csv
+    fCmd=${outdir}/cmd_test_${id}.sh
+    echo $line > ${fCmd}
+    bash ${fCmd} > ${fPerf}
+    if [[ $id == "0" ]]; then
+        cmd_metric="TestCommand"
+	perf_metric="Wall-time,units"
+        echo "${cmd_metric},${perf_metric}" > ${fSummary}
+    fi
+    cmd_data=`tail -n 1 ${fCmd} | sed -e 's/\r//g'`
+    perf_data=`grep -ir "Wall-clock Time" ${fPerf} | sed -e 's/.*:\s*//g' | sed -e 's/\s\s*/,/g'`
+    echo "\"$cmd_data\",${perf_data}" | tee -a ${fSummary}
+    id=$((id+1))
+done
